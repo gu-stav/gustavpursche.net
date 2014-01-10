791d7aab4cdeef846c7d8f34042af185dc979d14
Gustav Pursche <gustavpursche@gmail.com>
2014-01-10 19:59:33 +0100

    added css resources;

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var app = express();
 
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use('/static', express.static(__dirname + '/static'));
+
 app.get('/', function(req, res) {
-  res.send('Hello World!!!!1');
+  res.render('index');
 });
 
-var port = process.env.PORT || 5000;
-
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen( process.env.PORT || 5000 );
